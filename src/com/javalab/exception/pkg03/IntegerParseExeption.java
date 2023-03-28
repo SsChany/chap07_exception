92da594b9e679e7ad3aa266b2170c3c887729945
SsChany <mashimaro501@naver.com>
2023-03-28 17:13:15 +0900

    chap07_exception

---
+++ src/com/javalab/exception/pkg03/IntegerParseExeption.java
@@ -0,0 +1,27 @@
+package com.javalab.exception.pkg03;
+/*
+ * 다중 예외 처리 ( 두가지 이상의 실행 예외 처리)
+ * -ArrayIndexOutOfBoundsException 예외 처리
+ * -Integer.parseint 예외처리
+ * -Exception은 예외의 최고 조상으로 모든 예외 처리 가능함.
+ */
+public class IntegerParseExeption {
+	public static void main(String[] args) {
+		String[] array = {"100", "1oo"};
+		
+		for (int i = 0; i <= array.length; i++) {	//Array length 초과
+			try {
+				int value = Integer.parseInt(array[i]);
+				System.out.println("array[" + i + "]:" + value);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("배열 인덱스가 초과됨: " + e.getMessage());
+			}catch (NumberFormatException e) {
+				System.out.println("숫자로 변환할 수 없음:" + e.getMessage());
+			}catch (Exception e) {
+				System.out.println("예외의 최고 조상 내가 다 잡는다.");
+			}finally {
+				System.out.println("다시실행하세요.");
+			}
+		}
+	}
+}
